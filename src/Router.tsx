7434b85d64f6cb6b14a245fb5776268c14e198c1
Bruno Carvalho <bruno@brunocarvalho.me>
2024-11-29 12:51:51 -0300

    Fix basename

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,24 +3,29 @@ import ProtectedRoute from './components/ProtectedRoute';
 import Dashboard from './pages/Dashboard.page';
 import Login from './pages/Login.page';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: (
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '*',
+      element: <p>Not Found</p>,
+    },
+  ],
   {
-    path: '/',
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '*',
-    element: <p>Not Found</p>,
-  },
-]);
+    basename: import.meta.env.VITE_BASE_URL,
+  }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
-  },
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    base: process.env.VITE_BASE_URL,
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.mjs',
+    },
+  });
+};
