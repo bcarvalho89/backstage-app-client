d6b6c23cd6ef8f1a3912c3c9b7c59c65d90d7d9c
Bruno Carvalho <bruno@brunocarvalho.me>
2024-12-04 15:29:49 -0300

    Embed token in graphql requests

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,9 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:4000';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: apiUrl,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
