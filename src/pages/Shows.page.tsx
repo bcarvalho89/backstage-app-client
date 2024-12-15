d4dbbd77feacb73fbd4dcb0beec61b0d59d65030
Bruno Carvalho <bruno@brunocarvalho.me>
2024-12-15 11:39:09 -0300

    Integrate Empty component in Shows page

--- src/pages/Shows.page.tsx
+++ src/pages/Shows.page.tsx
@@ -1,6 +1,31 @@
+import { IconCalendarEvent } from '@tabler/icons-react';
+import { useNavigate } from 'react-router-dom';
 import { Title } from '@mantine/core';
+import Empty from '@/components/Empty/Empty';
 
 export default function Shows() {
+  const hasShows = false;
+  const navigate = useNavigate();
+
+  if (!hasShows) {
+    const handleActionClick = () => {
+      navigate('shows/new');
+    };
+
+    return (
+      <>
+        <Title mb="lg">Apresentações</Title>
+
+        <Empty
+          icon={<IconCalendarEvent />}
+          text="Ao adicionar uma apresentação você poderá gerenciar pesquisas."
+          title="Você não tem apresentações"
+          action={{ onClick: handleActionClick, text: 'Adicionar apresentação' }}
+        />
+      </>
+    );
+  }
+
   return (
     <>
       <Title>Apresentações</Title>
