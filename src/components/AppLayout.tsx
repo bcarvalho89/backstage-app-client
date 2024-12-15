4b413113d099cf27d7de134ecf9bce04390a77ae
Bruno Carvalho <bruno@brunocarvalho.me>
2024-12-15 11:59:49 -0300

    Use AppShell from mantine

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { useAuth } from '@/hooks';
-import Sidebar from './Layout/Sidebar/Sidebar';
+import Navigation from './Layout/Navigation/Navigation';
 import Topbar from './Layout/Topbar/Topbar';
-import classes from './AppLayout.module.css';
 
 export default function AppLayout() {
   const { loading, user } = useAuth();
+  const [opened, { toggle }] = useDisclosure();
 
   if (loading) {
     return <p>Loading...</p>;
@@ -16,13 +18,21 @@ export default function AppLayout() {
   }
 
   return (
-    <>
-      <Topbar />
-      <Sidebar />
-
-      <main className={classes.main}>
+    <AppShell
+      header={{ height: 80 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Topbar />
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+      </AppShell.Header>
+      <AppShell.Navbar px="md">
+        <Navigation />
+      </AppShell.Navbar>
+      <AppShell.Main>
         <Outlet />
-      </main>
-    </>
+      </AppShell.Main>
+    </AppShell>
   );
 }
