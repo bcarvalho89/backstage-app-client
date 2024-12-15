777e8830d3ff06803e343cb147b60d4af6b4fd7e
Bruno Carvalho <bruno@brunocarvalho.me>
2024-12-15 11:38:57 -0300

    Integrate Empty component in Dashboard page

--- src/pages/Dashboard.page.tsx
+++ src/pages/Dashboard.page.tsx
@@ -1,9 +1,17 @@
+import { IconGauge } from '@tabler/icons-react';
 import { Title } from '@mantine/core';
+import Empty from '@/components/Empty/Empty';
 
 export default function Dashboard() {
   return (
     <>
       <Title>Dashboard</Title>
+
+      <Empty
+        icon={<IconGauge />}
+        text="Não temos informações para serem exibidas"
+        title="Sem informações"
+      />
     </>
   );
 }
