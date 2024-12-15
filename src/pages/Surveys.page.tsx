799c9a5401f2b92f2ddc28537b1a26b4bf97d1ac
Bruno Carvalho <bruno@brunocarvalho.me>
2024-12-15 11:39:17 -0300

    Integrate Empty component in Surveys page

--- src/pages/Surveys.page.tsx
+++ src/pages/Surveys.page.tsx
@@ -1,6 +1,31 @@
+import { IconAward } from '@tabler/icons-react';
+import { useNavigate } from 'react-router-dom';
 import { Title } from '@mantine/core';
+import Empty from '@/components/Empty/Empty';
 
 export default function Surveys() {
+  const hasSurveys = false;
+  const navigate = useNavigate();
+
+  if (!hasSurveys) {
+    const handleActionClick = () => {
+      navigate('surveys/new');
+    };
+
+    return (
+      <>
+        <Title mb="lg">Pesquisas de Satisfação</Title>
+
+        <Empty
+          icon={<IconAward />}
+          text="Pesquisas de satisfação são uma ótima maneira de mensurar a qualidade da banda"
+          title="Você não tem pesquisas"
+          action={{ onClick: handleActionClick, text: 'Adicionar pesquisa' }}
+        />
+      </>
+    );
+  }
+
   return (
     <>
       <Title>Pesquisas de Satisfação</Title>
